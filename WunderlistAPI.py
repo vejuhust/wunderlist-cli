982dbfa541b8bbb3030e4f2ce6f943ceb5fc0294
vejuhust <vejuhust@gmail.com>
2014-07-13 23:21:36 +0800

    Encapsulate login part as class WunderlistAPI
    With login status and token

--- WunderlistAPI.py
+++ WunderlistAPI.py
@@ -11,71 +11,95 @@ from auth import email, password
 
 
 
-login_url = "https://api.wunderlist.com/login"
-login_headers = {
-    'Accept' : 'application/json',
-    'Accept-Encoding' : 'gzip,deflate',
-    'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Host' : 'api.wunderlist.com',
-    'Origin' : 'https://www.wunderlist.com',
-}
-token_filename = "token.json"
+class WunderlistAPI():
+    
+    # Constructor
+    def __init__(self, email, password):
+        # Status
+        self.is_login = False
+        self.account_email = email
+        self.account_password = password
+        self.token = ''
+        # Constants
+        self.login_url = "https://api.wunderlist.com/login"
+        self.login_headers = {
+            'Accept' : 'application/json',
+            'Accept-Encoding' : 'gzip,deflate',
+            'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Host' : 'api.wunderlist.com',
+            'Origin' : 'https://www.wunderlist.com',
+        }
+        self.tokenfile_name = "token.json"
 
 
-# Decode gzipped response content
-def decode_content(retval):
-    if retval.info().has_key('content-encoding'):
-        fileobj = StringIO.StringIO()
-        fileobj.write(retval.read())
-        fileobj.seek(0)
-        gzip_file = gzip.GzipFile(fileobj=fileobj)
-        content = gzip_file.read()
-    else:
-        content = retval.read()
-    return content
+    # Save token in a file
+    def tokenfile_save(self, content):
+        file = open(self.tokenfile_name, "w")
+        file.write(content)
+        file.close()
 
 
-# Save token in a file
-def token_save(content):
-    file = open(token_filename, "w")
-    file.write(content)
-    file.close()
+    # Load token from a file
+    def tokenfile_load(self):
+        file = open(self.tokenfile_name, "r")
+        content = file.read()
+        file.close()
+        return json.loads(content)
 
 
-# Load token from a file
-def token_load():
-    file = open(token_filename, "r")
-    content = file.read()
-    file.close()
-    return json.loads(content)
+    # Decode gzipped response content
+    def decode_content(self, retval):
+        if retval.info().has_key('content-encoding'):
+            fileobj = StringIO.StringIO()
+            fileobj.write(retval.read())
+            fileobj.seek(0)
+            gzip_file = gzip.GzipFile(fileobj=fileobj)
+            content = gzip_file.read()
+        else:
+            content = retval.read()
+        return content
 
 
-# Login with account and password
-def login(email, password):
-    data = {}
-    login_data = { 'email' : email, 'password' : password }
-    login_request = urllib2.Request(url = login_url, headers = login_headers, data = urllib.urlencode(login_data))
-    try:
-        login_response = urllib2.urlopen(login_request, timeout = 5)
-    except urllib2.URLError, error:
-        error_code = error.code if hasattr(error, 'code') else ''
-        error_reason = error.reason if hasattr(error, 'reason') else ''
-        print "Error:", error_code, error_reason
-    except Exception as error:
-        print "Error:", error
-    else:
-        content = decode_content(login_response)
-        token_save(content)
-        data = json.loads(content)
-    return data
+    # Login with account and password
+    def wunderlist_login(self, email, password):
+        data = {}
+        login_data = { 'email' : self.account_email, 'password' : self.account_password }
+        login_request = urllib2.Request(url = self.login_url, headers = self.login_headers, data = urllib.urlencode(login_data))
+        try:
+            login_response = urllib2.urlopen(login_request, timeout = 5)
+        except urllib2.URLError, error:
+            error_code = error.code if hasattr(error, 'code') else ''
+            error_reason = error.reason if hasattr(error, 'reason') else ''
+            print "Login Error:", error_code, error_reason
+        except Exception as error:
+            print "Login Error:", error
+        else:
+            content = self.decode_content(login_response)
+            self.tokenfile_save(content)
+            data = json.loads(content)
+        return data
 
 
-try:
-    data = token_load()
-except Exception as error:
-    data = login(email, password)
-finally:
-    print json.dumps(data, sort_keys = True, indent = 4)
+    # Check login status and get token
+    def login(self):
+        if not self.is_login or len(self.token) == 0:
+            try:
+                data = self.tokenfile_load()
+                self.token = data['token']
+            except Exception as error:
+                print "Load Error:", error
+                data = self.wunderlist_login(email, password)
+                self.token = data['token'] if data.has_key('token') else ''
+            finally:
+                if len(self.token) > 0:
+                    self.is_login = True
+                #print json.dumps(data, sort_keys = True, indent = 4)
 
 
-exit(0)
+
+api = WunderlistAPI(email, password)
+print api.token
+api.login()
+print api.is_login
+print api.token
+
